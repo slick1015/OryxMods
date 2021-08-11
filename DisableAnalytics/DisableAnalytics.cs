dc95689cbb26f76f8c5d67c6e5b12b57fce6abcc
Slick <jacobfarrell18@gmail.com>
2021-08-10 22:49:12 -0500

    Add DisableAnalytics mod

---
+++ DisableAnalytics/DisableAnalytics.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeltaDNA;
+using HarmonyLib;
+
+namespace DisableAnalytics
+{
+    public class DisableAnalytics : MelonMod
+    {
+        [HarmonyPatch(typeof(DDNA))]
+        class DDNA_Patches
+        {
+            [HarmonyPatch("Awake")]
+            [HarmonyPrefix]
+            static bool Awake_Prefix() => false;
+        }
+    }
+}
