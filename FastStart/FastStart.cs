a2c136c6c9877f6961010d4f7ee67adb01dbe78b
Slick <jacobfarrell18@gmail.com>
2021-08-10 22:43:24 -0500

    Add FastStart mod. Just skips SplashScreen right now

---
+++ FastStart/FastStart.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace FastStart
+{
+    public class FastStart : MelonMod
+    {
+        [HarmonyPatch(typeof(SplashScreenScript))]
+        class SplashScreenScript_Patches
+        {
+            [HarmonyPatch("Start")]
+            [HarmonyPrefix]
+            static bool Start_Prefix()
+            {
+                SceneManager.LoadScene("Bootstrap");
+                return false;
+            }
+        }
+    }
+}
