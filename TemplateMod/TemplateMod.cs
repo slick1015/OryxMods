8af675f4de447b9887d848847682519c8068002a
Slick <jacobfarrell18@gmail.com>
2021-08-10 22:19:24 -0500

    Add project files.

---
+++ TemplateMod/TemplateMod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+namespace TemplateMod
+{
+    public class TemplateMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Hello from Template Mod!");
+        }
+    }
+}
